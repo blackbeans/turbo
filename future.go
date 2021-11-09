71b0555c39e835090f40b96c2b53fc97d4875d56
Beta版厨子3.0® <blackbeans.zc@gmail.com>
2021-11-09 12:29:43 +0800

    update future.go
    add  cancel function for  FutureTask.

--- future.go
+++ future.go
@@ -7,6 +7,10 @@ import (
 
 //future task
 type FutureTask struct {
+	
+	ctx        context.Context
+	cancelFunc context.CancelFunc
+	
 	wg   *sync.WaitGroup
 	once *sync.Once
 	do   func(ctx context.Context) (interface{}, error)
@@ -27,6 +31,7 @@ func NewFutureTask(do func(ctx context.Context) (interface{}, error)) *FutureTas
 
 func (self *FutureTask) Run(ctx context.Context) {
 	self.once.Do(func() {
+		self.ctx, self.cancelFunc = context.WithCancel(ctx)
 		if nil != self.do {
 			select {
 			case <-ctx.Done():
@@ -44,3 +49,12 @@ func (self *FutureTask) Get() (interface{}, error) {
 	self.wg.Wait()
 	return self.result, self.err
 }
+
+
+//取消任务执行
+//future直接结束了
+func (self *FutureTask) Cancel() {
+	//取消任务执行
+	self.cancelFunc()
+}
+
